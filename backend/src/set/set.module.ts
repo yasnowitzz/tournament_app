43139563a5c67834f598460efd30acfb408c2a6a
Tomasz Lis <tlis@riskalyze.com>
2025-02-18 22:12:08 +0100

    feat: update team to next match

--- backend/src/set/set.module.ts
+++ backend/src/set/set.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Set } from './set.entity';
+import { Match } from '../match/match.entity';
 import { SetService } from './set.service';
 import { SetController } from './set.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Set])],
+  imports: [TypeOrmModule.forFeature([Set, Match])],
   providers: [SetService],
   controllers: [SetController],
-  exports: [SetService], // Udostępniamy SetService dla innych modułów
+  exports: [SetService, TypeOrmModule], // Udostępniamy SetService dla innych modułów
 })
 export class SetModule {}
